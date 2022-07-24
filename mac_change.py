5447249f3745bf295ef925ff092c20bfefa42670
abraham <abraham>
2022-07-24 03:57:06 +0100

    changed functionality of code to work with python3

--- mac_change.py
+++ mac_change.py
@@ -2,14 +2,14 @@
 
 import subprocess
 import re
-import optparse
+import argparse
 
 
 def take_arg():
-    collect = optparse.OptionParser()
-    collect.add_option("-i", "--interface", dest="interface", help="The interface to change it's MAC address")
-    collect.add_option("-m", "--mac", dest="address", help="The MAC address to be used")
-    (option, arguments) = collect.parse_args()
+    collect = argparse.ArgumentParser()
+    collect.add_argument("-i", "--interface", dest="interface", help="The interface to change it's MAC address")
+    collect.add_argument("-m", "--mac", dest="address", help="The MAC address to be used")
+    option = collect.parse_args()
     if not option.interface:
         collect.error("[-] Kindly specify the interface or use --help for more information")
     elif not option.address:
@@ -20,7 +20,8 @@ def take_arg():
 
 def get_current_mac(interface):
     get_mac = subprocess.check_output(["ifconfig", interface])
-    get_mac2 = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", get_mac)
+    get_mac2 = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", get_mac.decode("utf-8"))
+    # .decode("utf-8) will change the result from bytes to string for python3 to work it
     former_mac = get_mac2.group(0)
     if not former_mac:
         print("[-] Could not find MAC address")
@@ -40,19 +41,17 @@ def change_mac(interface, address, form):
 
 def get_new_mac(interface, address, form):
     new_mac = subprocess.check_output(["ifconfig", interface])
-    new_mac2 = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", new_mac)
+    new_mac2 = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", new_mac.decode("utf-8"))
     changed_mac = new_mac2.group(0)
     if changed_mac == address:
         print("[+] MAC address has successfully been changed from " + form + " to " + address)
     else:
         print("[-] Failed to change MAC address from " + form + " to " + address)
         print("[-] The code needs a super-user permission, kindly run in root")
-        print("[-] Run the code using python2 instead of python3")
         print("[-] Try using another address")
 
 
 print("This code should be run in python2, python3 has difficulties in processing the code.")
-print("Thank you, God bless you!")
 options = take_arg()
 forms = get_current_mac(options.interface)
 change_mac(options.interface, options.address, forms)
